8894c2397594d3eea53191c8f55d66dac7db3785
shohjahon98 <55964240+shohjahon98@users.noreply.github.com>
2021-11-25 13:42:27 +0500

    Sandbox(Sample Scene)
    Draw a Ray for the main camera...

---
+++ RPG_6BISP/Assets/Scripts/Mover.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+    Ray lastRay;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
+        Debug.DrawRay(lastRay.origin, lastRay.direction * 100);
+        GetComponent<NavMeshAgent>().destination = target.position;
+    }
+}
