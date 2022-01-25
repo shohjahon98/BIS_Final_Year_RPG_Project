0dcd5d2353a86ea751691afdeba760d8fa9ce3bc
shohjahon98 <55964240+shohjahon98@users.noreply.github.com>
2022-01-25 18:38:53 +0500

    Combat
    Fighter attack is implemented!

--- RPG_6BISP/Assets/Scripts/Movement/Mover.cs
+++ RPG_6BISP/Assets/Scripts/Movement/Mover.cs
@@ -3,34 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class Mover : MonoBehaviour
+namespace RPG.Movement 
 {
-    [SerializeField] Transform target;
-    void Update()
+    public class Mover : MonoBehaviour
     {
-        if (Input.GetMouseButton(0))
+        [SerializeField] Transform target;
+        void Update()
         {
-            MoveToCursor();
+            UpdateAnimator();
         }
-        UpdateAnimator();
-    }
+        public void MoveTo(Vector3 destination)
+        {
+            GetComponent<NavMeshAgent>().destination = destination;
+        }
 
-    private void MoveToCursor()
-    {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        bool hasHit = Physics.Raycast(ray, out hit);
-        if (hasHit)
+        private void UpdateAnimator()
         {
-            GetComponent<NavMeshAgent>().destination = hit.point;
+            Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+            Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+            float speed = localVelocity.z;
+            GetComponent<Animator>().SetFloat("forwardSpeed", speed);
         }
-    }
-    private void UpdateAnimator()
-    {
-        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
-        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
-        float speed = localVelocity.z;
-        GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+
     }
 
 }
