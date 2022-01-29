f43af011887ed1a4ef284a5b25ed684bab9becec
shohjahon98 <55964240+shohjahon98@users.noreply.github.com>
2022-01-29 23:56:38 +0500

    Enemy
    The first thing I have created the path "PatrolPath" that the enemy walk around and the Second tricky is that The enemy can walk along it...

--- RPG_6BISP/Assets/Scripts/Control/AIController.cs
+++ RPG_6BISP/Assets/Scripts/Control/AIController.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections;
+﻿using RPG.Combat;
+using RPG.Core;
+using RPG.Movement;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,20 +10,78 @@ namespace RPG.Control
     public class AIController : MonoBehaviour
     {
         [SerializeField] float chaseDistance = 5f;
+        [SerializeField] float suspicionTime = 3f;
+        [SerializeField] PatrolPath patrolPath;
+
+        Fighter fighter;
+        Health health;
+        Mover mover;
+        GameObject player;
+
+        Vector3 guardPosition;
+        float timeSinceLastSawPlayer = Mathf.Infinity;
+
+        private void Start()
+        {
+            fighter = GetComponent<Fighter>();
+            health = GetComponent<Health>();
+            mover = GetComponent<Mover>();
+            player = GameObject.FindWithTag("Player");
+
+            guardPosition = transform.position;
+        }
 
         private void Update()
         {
-            if (DistanceToPlayer() < chaseDistance)
+            if (health.IsDead()) return;
+
+            if (InAttackRangeOfPlayer() && fighter.CanAttack(player))
             {
-                print(gameObject.name + " Should chase");
+                timeSinceLastSawPlayer = 0;
+                AttackBehaviour();
             }
+            else if(timeSinceLastSawPlayer < suspicionTime)
+            {
+                //Suspicion state
+                SuspicionBehaviour();
+            }
+            else
+            {
+                GuardBehaviour();
+            }
+
+            timeSinceLastSawPlayer += Time.deltaTime;
         }
 
-        private float DistanceToPlayer()
+        private void GuardBehaviour()
         {
-            GameObject player = GameObject.FindWithTag("Player");
-            return Vector3.Distance(player.transform.position, transform.position);
+            mover.StartMoveAction(guardPosition);
         }
+
+        private void SuspicionBehaviour()
+        {
+            GetComponent<ActionScheduler>().CancelCurrentAction();
+        }
+
+        private void AttackBehaviour()
+        {
+            fighter.Attack(player);
+        }
+
+        private bool InAttackRangeOfPlayer()
+        {
+
+            float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+            return distanceToPlayer < chaseDistance;
+        }
+        // Called by Unity
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(transform.position, chaseDistance);
+        }
+
+
     }
 
 }
--- RPG_6BISP/Assets/Scripts/Control/PlayerController.cs
+++ RPG_6BISP/Assets/Scripts/Control/PlayerController.cs
@@ -1,4 +1,5 @@
 ﻿using RPG.Combat;
+using RPG.Core;
 using RPG.Movement;
 using System;
 using System.Collections;
@@ -9,8 +10,15 @@ namespace RPG.Control
 {
     public class PlayerController : MonoBehaviour
     {
+        Health health;
+        private void Start()
+        {
+            health = GetComponent<Health>();
+        }
         private void Update()
         {
+            if (health.IsDead()) return;
+
             if (InteractWithCombat()) return;
             if (InteractWithMovement()) return;
         }
@@ -23,7 +31,6 @@ namespace RPG.Control
                CombatTarget target = hit.transform.GetComponent<CombatTarget>();
                 if (target == null) continue;
 
-                GameObject targetGameObject = target.gameObject;
                 if (!GetComponent<Fighter>().CanAttack(target.gameObject))
                 {
                     continue;
