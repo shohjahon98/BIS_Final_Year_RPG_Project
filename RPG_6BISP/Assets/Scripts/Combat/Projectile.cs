e0b9551b09e4d7da9920c1ef3a9ffad7cbc27dbb
shohjahon98 <55964240+shohjahon98@users.noreply.github.com>
2022-02-16 17:49:30 +0500

    Arrow
    Arrow Projectile is used for the second weapon for the player and connected with bow.

---
+++ RPG_6BISP/Assets/Scripts/Combat/Projectile.cs
@@ -0,0 +1,35 @@
+﻿using RPG.Core;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] float speed = 1;
+
+    Health target = null;
+
+
+    void Update()
+    {
+        if (target == null) return;
+
+        transform.LookAt(GetAimLocation());
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    public void SetTarget(Health target)
+    {
+        this.target = target;
+    }
+
+    private Vector3 GetAimLocation()
+    {
+        CapsuleCollider targetCapsule = target.GetComponent<CapsuleCollider>();
+        if(targetCapsule == null)
+        {
+            return target.transform.position;
+        }
+        return target.transform.position + Vector3.up * targetCapsule.height / 2;
+    }
+}
--- RPG_6BISP/Assets/Scripts/Combat/Weapon.cs
+++ RPG_6BISP/Assets/Scripts/Combat/Weapon.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using RPG.Core;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,23 +12,42 @@ namespace RPG.Combat
         [SerializeField] GameObject equippedPrefab = null;
         [SerializeField] float weaponDamage = 5f;
         [SerializeField] float weaponRange = 2f;
-        [SerializeField] bool isRightHanded = true; 
+        [SerializeField] bool isRightHanded = true;
+        [SerializeField] Projectile projectile = null;
 
         public void Spawn(Transform rightHand, Transform leftHand, Animator animator)
         {
             if (equippedPrefab != null)
             {
-                Transform handTransform;
-                if (isRightHanded) handTransform = rightHand;
-                else handTransform = leftHand;
+                Transform handTransform = GetTransform(rightHand, leftHand);
                 Instantiate(equippedPrefab, handTransform);
             }
-            if(animatorOverride != null)
+            if (animatorOverride != null)
             {
                 animator.runtimeAnimatorController = animatorOverride;
 
             }
         }
+
+        private Transform GetTransform(Transform rightHand, Transform leftHand)
+        {
+            Transform handTransform;
+            if (isRightHanded) handTransform = rightHand;
+            else handTransform = leftHand;
+            return handTransform;
+        }
+
+        public bool HasProjectile()
+        {
+            return projectile != null;
+        }
+
+        public void LaunchProjectile(Transform rightHand, Transform leftHand, Health target) 
+        {
+            Projectile projectileInstance = Instantiate(projectile, GetTransform(rightHand, leftHand).position, Quaternion.identity);
+            projectileInstance.SetTarget(target);
+        }
+
 
         public float GetDamage()
         {
