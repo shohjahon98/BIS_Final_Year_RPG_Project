137e36324b80c5b9d46e2ee955d10089d653d9ad
shohjahon98 <55964240+shohjahon98@users.noreply.github.com>
2022-02-24 11:03:31 +0500

    The weapon projectile fixed
    the bow elements fixed now!

--- RPG_6BISP/Assets/Scripts/Combat/Projectile.cs
+++ RPG_6BISP/Assets/Scripts/Combat/Projectile.cs
@@ -6,16 +6,24 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     [SerializeField] float speed = 1;
-
+    [SerializeField] bool isHoming = true;
     Health target = null;
     float damage = 0;
 
+    private void Start()
+    {
+        transform.LookAt(GetAimLocation());
+
+    }
 
     void Update()
     {
         if (target == null) return;
+        if (isHoming && !target.IsDead())
+        {
+            transform.LookAt(GetAimLocation());
 
-        transform.LookAt(GetAimLocation());
+        }
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
     }
 
@@ -38,6 +46,7 @@ public class Projectile : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<Health>() != target) return;
+        if (target.IsDead()) return;
         target.TakeDamage(damage);
         Destroy(gameObject);
     }
