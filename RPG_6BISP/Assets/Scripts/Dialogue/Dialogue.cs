6e54540f3808915f472786a3c8de4995021b2bf9
shohjahon98 <55964240+shohjahon98@users.noreply.github.com>
2022-03-16 15:22:01 +0500

    Unused assets Deleted and Dialogue system
    The Dialogue system implemented for the project!

---
+++ RPG_6BISP/Assets/Scripts/Dialogue/Dialogue.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Dialogue
+{
+    [CreateAssetMenu(fileName = "New Dialogue", menuName = "Dialogue", order = 0)]
+    public class Dialogue : ScriptableObject
+    {
+        [SerializeField]
+        List<DialogueNode> nodes;
+#if UNITY_EDITOR
+        private void Awake()
+        {
+            if(nodes.Count == 0)
+            {
+                nodes.Add(new DialogueNode());
+            }
+        }
+#endif
+        public IEnumerable<DialogueNode> GetAllNodes()
+        {
+            return nodes;
+        }
+        public DialogueNode GetRootNode()
+        {
+            return nodes[0];
+        }
+
+        public IEnumerable<DialogueNode> GetAllChildren(DialogueNode parentNode)
+        {
+            foreach (string childID in parentNode.GetChildren())
+            {
+                if (nodeLookup.ContainsKey(childID))
+                {
+                    yield return nodeLookup[childID];
+                }
+            }
+        }
+    }
+}
