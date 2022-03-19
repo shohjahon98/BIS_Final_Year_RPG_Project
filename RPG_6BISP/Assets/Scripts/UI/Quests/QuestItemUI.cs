b06c66dab21b12124eaaec587232407bb47cdb02
shohjahon98 <55964240+shohjahon98@users.noreply.github.com>
2022-03-19 16:49:45 +0500

    2 Quests created
    N/A

--- RPG_6BISP/Assets/Scripts/UI/Quests/QuestItemUI.cs
+++ RPG_6BISP/Assets/Scripts/UI/Quests/QuestItemUI.cs
@@ -1,25 +1,27 @@
-﻿//using System.Collections;
-//using System.Collections.Generic;
-//using RPG.Quests;
-//using TMPro;
-//using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using RPG.Quests;
+using TMPro;
+using UnityEngine;
 
-//public class QuestItemUI : MonoBehaviour
-//{
-//    [SerializeField] TextMeshProUGUI title;
-//    [SerializeField] TextMeshProUGUI progress;
+public class QuestItemUI : MonoBehaviour
+{
+     [SerializeField] TextMeshProUGUI title;
+     [SerializeField] TextMeshProUGUI progress;
 
 //    QuestStatus status;
 
-//    public void Setup(QuestStatus status)
-//    {
-//        this.status = status;
-//        title.text = status.GetQuest().GetTitle();
-//        progress.text = status.GetCompletedCount() + "/" + status.GetQuest().GetObjectiveCount();
-//    }
+     public void Setup(Quest quest)
+     {
+        title.text = quest.GetTitle();
+        progress.text = "0/" + quest.GetObjectiveCount();
+         //this.status = status;
+         //title.text = status.GetQuest().GetTitle();
+         //progress.text = status.GetCompletedCount() + "/" + status.GetQuest().GetObjectiveCount();
+    }
 
 //    public QuestStatus GetQuestStatus()
 //    {
 //        return status;
 //    }
-//}
+}
--- RPG_6BISP/Assets/Scripts/UI/Quests/QuestListUI.cs
+++ RPG_6BISP/Assets/Scripts/UI/Quests/QuestListUI.cs
@@ -1,31 +1,33 @@
-﻿//using System.Collections;
-//using System.Collections.Generic;
-//using RPG.Quests;
-//using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using RPG.Quests;
+using UnityEngine;
 
-//public class QuestListUI : MonoBehaviour
-//{
-//    [SerializeField] QuestItemUI questPrefab;
-//    QuestList questList;
-    
-//    // Start is called before the first frame update
-//    void Start()
-//    {
-//        questList = GameObject.FindGameObjectWithTag("Player").GetComponent<QuestList>();
-//        questList.onUpdate += Redraw;
-//        Redraw();
-//    }
+public class QuestListUI : MonoBehaviour
+{
+    [SerializeField] Quest[] tempQuests;
+    [SerializeField] QuestItemUI questPrefab;
+    //QuestList questList;
 
-//    private void Redraw()
-//    {
-//        foreach (Transform item in transform)
-//        {
-//            Destroy(item.gameObject);
-//        }
-//        foreach (QuestStatus status in questList.GetStatuses())
-//        {
-//            QuestItemUI uiInstance = Instantiate<QuestItemUI>(questPrefab, transform);
-//            uiInstance.Setup(status);
-//        }
-//    }
-//}
+    // Start is called before the first frame update
+    //void Start()
+    //{
+    //    questList = GameObject.FindGameObjectWithTag("Player").GetComponent<QuestList>();
+    //    questList.onUpdate += Redraw;
+    //    Redraw();
+    //}
+
+       void Start()
+       {
+        //foreach (Transform item in transform)
+        //{
+        //   Destroy(item.gameObject);
+        //}
+        transform.DetachChildren();
+        foreach (Quest quest in tempQuests)
+        {
+            QuestItemUI uiInstance = Instantiate<QuestItemUI>(questPrefab, transform);
+            uiInstance.Setup(quest);
+        }
+    }
+}
